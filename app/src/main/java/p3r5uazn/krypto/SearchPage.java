20df5b5ff99aa02fda8c4052225ee734a4b5356f
delacruzkb <delacruzkb@g.cofc.edu>
2018-03-11 18:15:15 -0400

    added a search page

---
+++ app/src/main/java/p3r5uazn/krypto/SearchPage.java
@@ -0,0 +1,13 @@
+package p3r5uazn.krypto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_screenx);
+    }
+}
---
+++ app/src/main/java/p3r5uazn/krypto/SearchScreenListAdapter.java
@@ -0,0 +1,8 @@
+package p3r5uazn.krypto;
+
+/**
+ * Created by kenso on 3/11/2018.
+ */
+
+public class SearchScreenListAdapter {
+}
