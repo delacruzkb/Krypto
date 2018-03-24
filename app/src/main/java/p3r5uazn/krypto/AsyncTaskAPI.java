37b2b8a84fe572707113e788b7769556ebc1b7e7
Shaina Mae Mainar <shainamainar@gmail.com>
2018-03-23 21:56:37 -0400

    asyncAPI returns void

--- app/src/main/java/p3r5uazn/krypto/AsyncTaskAPI.java
+++ app/src/main/java/p3r5uazn/krypto/AsyncTaskAPI.java
@@ -6,5 +6,9 @@ import android.os.AsyncTask;
  * Created by smainar on 3/17/2018.
  */
 
-public class AsyncTaskAPI extends AsyncTask<> {
+public class AsyncTaskAPI extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
 }
